26110c19009c998cf7c0c520e3af77bdbf779fb6
zhouxsxc <zhouxsxc@163.com>
2022-04-18 16:38:16 +0800

    add fuji

--- frontend/cyberpop-bridge-release/src/page/cyt/chooseAddressForm/chainSelect.tsx
+++ frontend/cyberpop-bridge-release/src/page/cyt/chooseAddressForm/chainSelect.tsx
@@ -3,36 +3,41 @@ import type { SelectProps } from '@arco-design/web-react'
 import type { FC } from 'react'
 
 const chains: Record<string, any> = {
-  "ethereum": {
-    "symbol": "Ethereum Mainnet",
-    "chainId": 1,
-    "logo": "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=022"
-  },
-  "rinkeby": {
-    "symbol": "Rinkeby",
-    "chainId": 4,
-    "logo": "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=022"
-  },
+  // "ethereum": {
+  //   "symbol": "Ethereum Mainnet",
+  //   "chainId": 1,
+  //   "logo": "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=022"
+  // },
+  // "rinkeby": {
+  //   "symbol": "Rinkeby",
+  //   "chainId": 4,
+  //   "logo": "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=022"
+  // },
   "mumbai": {
     "symbol": "Mumbai",
     "chainId": 80001,
     "logo": "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=022"
   },
-  "heco": {
-    "symbol": "Huobi ECO Chain Mainnet",
-    "chainId": 128,
-    "logo": "https://cryptologos.cc/logos/huobi-token-ht-logo.svg?v=022"
+  "fuji": {
+    "symbol": "Avalanche Fuji Testnet",
+    "chainId": 43113,
+    "logo": "	https://nftrade.com/img/chains/icons/avax.png"
   },
-  "polygon": {
-    "symbol": "Polygon Mainnet",
-    "chainId": 137,
-    "logo": "https://cryptologos.cc/logos/polygon-matic-logo.svg?v=022"
-  },
-  "ganache": {
-    "symbol": "Ganache",
-    "chainId": 1337,
-    "logo": "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=022"
-  }
+  // "heco": {
+  //   "symbol": "Huobi ECO Chain Mainnet",
+  //   "chainId": 128,
+  //   "logo": "https://cryptologos.cc/logos/huobi-token-ht-logo.svg?v=022"
+  // },
+  // "polygon": {
+  //   "symbol": "Polygon Mainnet",
+  //   "chainId": 137,
+  //   "logo": "https://cryptologos.cc/logos/polygon-matic-logo.svg?v=022"
+  // },
+  // "ganache": {
+  //   "symbol": "Ganache",
+  //   "chainId": 1337,
+  //   "logo": "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=022"
+  // }
 }
 
 export const ChainSelect: FC<SelectProps> = (props) => {
