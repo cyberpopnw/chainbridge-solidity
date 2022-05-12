d9854cb619daef2ce292fc8270f1b1f05a706389
tonori <tonori@tonori.cn>
2022-05-12 18:25:12 +0800

    fix: erc20 transfer

--- frontend/cyberpop-bridge-release/src/hooks/useCytDeposit.ts
+++ frontend/cyberpop-bridge-release/src/hooks/useCytDeposit.ts
@@ -1,13 +1,16 @@
 import { useGlobalStateContext } from '@/hooks/useGlobalStateContext'
 import { utils, BigNumber } from 'ethers'
 import waitForTransaction from '@/utils/waitForTransaction'
+import { getChain } from '@/lib/chainIds'
 
 export const useCytDeposit = () => {
   const { cyt, bridge } = useGlobalStateContext()
 
   return async (chainId: number | undefined, to: string, amount: number) => {
-    if (chainId == null) {
-      return Promise.reject({ message: 'Chain ID is required.' })
+    const targetChain = getChain(chainId)
+
+    if (!targetChain) {
+      return Promise.reject({ message: `Unknown Chain ID ${chainId}` })
     }
 
     const handler = await bridge?._resourceIDToHandlerAddress(process.env.REACT_APP_CytResourceID)
@@ -19,7 +22,7 @@ export const useCytDeposit = () => {
       to.substring(2);
 
     return waitForTransaction(
-      await bridge?.deposit(chainId === 80001 ? '0' : '1', process.env.REACT_APP_CytResourceID, data)
+      await bridge?.deposit(targetChain.bridgeId, process.env.REACT_APP_CytResourceID, data)
     )
   }
 }
